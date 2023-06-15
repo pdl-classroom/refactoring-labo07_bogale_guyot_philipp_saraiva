2b6db9636aee7a228177d9a13278592fc1b0c552
TegestBogale <tegest.g.bogale@gmail.com>
2023-06-15 17:57:57 +0200

    remove check for last element

--- src/main/java/ch/heigvd/pdl/refactoring/OrdersWriter.java
+++ src/main/java/ch/heigvd/pdl/refactoring/OrdersWriter.java
@@ -2,23 +2,20 @@ package ch.heigvd.pdl.refactoring;
 
 public class OrdersWriter {
 
-   private final Orders orders;
+    private final Orders orders;
 
-   public OrdersWriter(Orders orders) {
-      this.orders = orders;
-   }
-   public String getContents() {
-      StringBuffer sb = new StringBuffer("{\"orders\": [");
-      for (int i = 0; i < orders.getOrdersCount(); i++) {
+    public OrdersWriter(Orders orders) {
+        this.orders = orders;
+    }
+
+    public String getContents() {
+        StringBuffer sb = new StringBuffer("{\"orders\": [");
+        for (int i = 0; i < orders.getOrdersCount(); i++) {
             Order order = orders.getOrder(i);
             sb.append(order);
-            sb.append(", ");
-      }
+            sb.append(i == orders.getOrdersCount() - 1 ? "" : ", ");
+        }
 
-
-      if (orders.getOrdersCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
-      }
-      return sb.append("]}").toString();
-   }
+        return sb.append("]}").toString();
+    }
 }
