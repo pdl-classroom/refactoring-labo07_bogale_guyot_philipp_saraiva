334d33387ecc41af3182fa89134d5bf9fed95af4
leandro <leandro262009@gmail.com>
2023-06-15 16:47:57 +0200

    public to private in tests

--- src/test/java/ch/heigvd/pdl/refactoring/OrdersWriterTest.java
+++ src/test/java/ch/heigvd/pdl/refactoring/OrdersWriterTest.java
@@ -9,23 +9,23 @@ public class OrdersWriterTest {
     Order order111 = new Order(111);
 
     @BeforeEach
-    public void SetupOneOrder() {
+    private void SetupOneOrder() {
         orders.addOrder(order111);
     }
 
     @Test
-    public void NoOrder() {
+    private void NoOrder() {
         assertEquals("{\"orders\": []}", new OrdersWriter(new Orders()).getContents());
     }
 
     @Test
-    public void OneOrder() {
+    private void OneOrder() {
         String order111 = "{\"id\": 111, \"products\": []}";
         assertEquals("{\"orders\": [" + order111 + "]}", new OrdersWriter(orders).getContents());
     }
 
     @Test
-    public void TwoOrders() {
+    private void TwoOrders() {
         orders.addOrder(new Order(222));
         String order111Json = JsonOrder111WithProduct("");
         String order222Json = "{\"id\": 222, \"products\": []}";
@@ -33,14 +33,14 @@ public class OrdersWriterTest {
     }
 
     @Test
-    public void OneOrderWithOneProduct() {
+    private void OneOrderWithOneProduct() {
         order111.addProduct(new Product("Shirt", 1, 3, 2.99, "TWD"));
         String order111Json = JsonOrder111WithProduct("{\"code\": \"Shirt\", \"color\": \"blue\", \"size\": \"M\", \"price\": 2.99, \"currency\": \"TWD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", new OrdersWriter(orders).getContents());
     }
 
     @Test
-    public void OneOrderWithOneProductNoSize() {
+    private void OneOrderWithOneProductNoSize() {
         order111.addProduct(new Product("Pot", 2, -1, 16.50, "SGD"));
         String order111Json = JsonOrder111WithProduct("{\"code\": \"Pot\", \"color\": \"red\", \"price\": 16.5, \"currency\": \"SGD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", new OrdersWriter(orders).getContents());
