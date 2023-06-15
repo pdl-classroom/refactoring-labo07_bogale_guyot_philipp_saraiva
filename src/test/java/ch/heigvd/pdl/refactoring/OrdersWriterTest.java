cf593684a879a104526224d750d8e652534b422c
leandro <leandro262009@gmail.com>
2023-06-15 17:26:59 +0200

    remade test public

--- src/test/java/ch/heigvd/pdl/refactoring/OrdersWriterTest.java
+++ src/test/java/ch/heigvd/pdl/refactoring/OrdersWriterTest.java
@@ -11,23 +11,23 @@ public class OrdersWriterTest {
     Order order111 = new Order(111);
 
     @BeforeEach
-    private void SetupOneOrder() {
+    public void SetupOneOrder() {
         orders.addOrder(order111);
     }
 
     @Test
-    private void NoOrder() {
+    public void NoOrder() {
         assertEquals("{\"orders\": []}", new OrdersWriter(new Orders()).getContents());
     }
 
     @Test
-    private void OneOrder() {
+    public void OneOrder() {
         String order111 = "{\"id\": 111, \"products\": []}";
         assertEquals("{\"orders\": [" + order111 + "]}", new OrdersWriter(orders).getContents());
     }
 
     @Test
-    private void TwoOrders() {
+    public void TwoOrders() {
         orders.addOrder(new Order(222));
         String order111Json = JsonOrder111WithProduct("");
         String order222Json = "{\"id\": 222, \"products\": []}";
@@ -35,14 +35,14 @@ public class OrdersWriterTest {
     }
 
     @Test
-    private void OneOrderWithOneProduct() {
+    public void OneOrderWithOneProduct() {
         order111.addProduct(new Product("Shirt", Color.BLUE, Size.M, 2.99, "TWD"));
         String order111Json = JsonOrder111WithProduct("{\"code\": \"Shirt\", \"color\": \"blue\", \"size\": \"M\", \"price\": 2.99, \"currency\": \"TWD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", new OrdersWriter(orders).getContents());
     }
 
     @Test
-    private void OneOrderWithOneProductNoSize() {
+    public void OneOrderWithOneProductNoSize() {
         order111.addProduct(new Product("Pot", Color.RED, Size.INVALID_SIZE, 16.50, "SGD"));
         String order111Json = JsonOrder111WithProduct("{\"code\": \"Pot\", \"color\": \"red\", \"price\": 16.5, \"currency\": \"SGD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", new OrdersWriter(orders).getContents());
